3388e3433c26e96b9b47528536dfb3fe577ed0b6
Lasha-Zhizhiashvili <Lashalovesbasketball927@gmail.com>
2022-12-18 12:07:36 +0400

    nonon

---
+++ js/java.js
@@ -0,0 +1,22 @@
+let numb = 25; 
+let str = 'string' 
+let arr = [54, 'sad'];
+let obj = {email:'blablabla@gmail.com'};
+let funs = function(){};
+let bool = true;
+let fals = false;
+let undef = undefined;
+let nan = NaN;
+let s ;
+
+console.log(obj);
+console.log(numb);
+console.log(str);
+console.log(arr);
+console.log(obj);
+console.log(funs);
+console.log(bool);
+console.log(fals);
+console.log(undef);
+console.log(nan);
+console.log(s);
