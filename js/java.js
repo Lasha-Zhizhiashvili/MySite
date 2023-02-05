533ec7800b7fc9f4c77ca2b786038fd07e00d3f3
Lasha-Zhizhiashvili <Lashalovesbasketball927@gmail.com>
2023-02-05 13:00:30 +0400

    c bjghv

--- js/java.js
+++ js/java.js
@@ -1,49 +1,91 @@
-let butt = document.getElementById('but')
-let hid = document.getElementById('hid')
+let but = document.getElementById('but')
+
+let butt = document.getElementById('butt')
+let butt1 = document.getElementById('butt1')
+
+let buttt = document.getElementById('buttt')
+let buttt1 = document.getElementById('buttt1')
+
+let first = document.getElementById('first')
+let second = document.getElementById('second')
+let third = document.getElementById('third')
+
+let final = document.getElementById('final')
+
+let ser1 = document.getElementById('ser1')
+let sor1 = document.getElementById('sor1')
+
+let ser2 = document.getElementById('ser2')
+let sor2 = document.getElementById('sor2')
+
+let ser3 = document.getElementById('ser3')
+let sor3 = document.getElementById('sor3')
+
+
+
+but.addEventListener('click', function(){
+    first.style.display = 'none'
+    second.style.display = 'flex'
+
+    ser1.style.color = 'gray'
+    sor1.style.backgroundColor = 'gray'
+
+    ser2.style.color = 'rgb(11, 8, 219)'
+    sor2.style.backgroundColor = 'rgb(11, 8, 219)'
+})
 
 butt.addEventListener('click', function(){
-    if (hid.style.display == 'none'){
-        hid.style.display = 'block'
-    }
-    else{
-        hid.style.display = 'none'
-    }
-   
+    first.style.display = 'flex'
+    second.style.display = 'none'
+
+    ser1.style.color = 'rgb(11, 8, 219)'
+    sor1.style.backgroundColor = 'rgb(11, 8, 219)'
+
+    ser2.style.color = 'gray'
+    sor2.style.backgroundColor = 'gray'
 })
-
-hid.addEventListener('click', function(){
-    document.getElementById('hid').style.display = 'none'
- })
-
-
-let butt1 = document.getElementById('butt1')
-let only = document.getElementById('only')
-let form = document.getElementById('reg')
 
 butt1.addEventListener('click', function(){
-    let value = only.value
-    let neww = document.createElement('div')
-    let newww = document.createElement('p')
-    neww.appendChild(newww)
-    form.appendChild(neww)
-    neww.style.width = '185px'
-    neww.style.backgroundColor = 'red'
-    newww.textContent = value
-    only.value = ''
-    newww.addEventListener('click', function(){
-        this.remove()
-    })
+    second.style.display = 'none'
+    third.style.display = 'flex'
+
+    ser2.style.color = 'gray'
+    sor2.style.backgroundColor = 'gray'
+    
+    ser3.style.color = 'rgb(11, 8, 219)'
+    sor3.style.backgroundColor = 'rgb(11, 8, 219)'
 })
 
-let tag = document.getElementById('tag')
-let butt2 = document.getElementById('butt2')
+buttt.addEventListener('click', function(){
+    second.style.display = 'flex'
+    third.style.display = 'none'
 
-tag.addEventListener('keypress', function(tag){
-    let value1 = tag.value
-    if(tag.charCode == 13){
-        let span = document.createElement('span')
-        span.textContent = value1
-        tag.value = ''
-        
-    }
+    ser2.style.color = 'rgb(11, 8, 219)'
+    sor2.style.backgroundColor = 'rgb(11, 8, 219)'
+    
+    ser3.style.color = 'gray'
+    sor3.style.backgroundColor = 'gray'
 })
+
+buttt1.addEventListener('click', function(){
+    third.style.display = 'none'
+    final.style.display = 'block'
+
+    ser1.style.color = 'rgb(11, 8, 219)'
+    sor1.style.backgroundColor = 'rgb(11, 8, 219)'
+
+    ser2.style.color = 'rgb(11, 8, 219)'
+    sor2.style.backgroundColor = 'rgb(11, 8, 219)'
+    
+    ser3.style.color = 'rgb(11, 8, 219)'
+    sor3.style.backgroundColor = 'rgb(11, 8, 219)'
+})
+
+
+let change = document.getElementById('colorchange')
+let numbs = document.getElementById('numbs')
+
+change.addEventListener('click', function(){
+    
+})
+
