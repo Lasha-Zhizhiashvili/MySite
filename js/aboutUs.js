392004b77bda6c6641bd613783e25b99687c2e34
Lasha-Zhizhiashvili <Lashalovesbasketball927@gmail.com>
2023-03-09 15:30:40 +0400

    mlnn

--- js/aboutUs.js
+++ js/aboutUs.js
@@ -1,55 +1,38 @@
-let scrol1 = document.getElementById('scrol1')
-let scrol2 = document.getElementById('scrol2')
-let scrol3 = document.getElementById('scrol3')
-let scrol4 = document.getElementById('scrol4')
+let track = document.querySelector('.box6')
+let slides = Array.from(track.children)
+let dotsnav = document.querySelector('.btn-div')
+let dots = Array.from(dotsnav.children)
 
-let reed = document.getElementById('reed')
-let reed1 = document.getElementById('reed1')
-let reed2 = document.getElementById('reed2')
-let reed3 = document.getElementById('reed3')
+let slidessize = slides[0].getBoundingClientRect().width
 
-let box6 = document.querySelector('.box6')
+let setSlidesPosition = (slide, index) =>{
+    slide.style.left = slidessize * index + 'px';
+}
 
-scrol1.addEventListener('click', function(){
-    scrol1.classList.remove('active')
-    scrol2.classList.remove('active')
-    scrol3.classList.remove('active')
-    scrol4.classList.remove('active')
-    this.classList.add('active')
+slides.forEach(setSlidesPosition);
 
-    let scrol = reed1.getBoundingClientRect().left
-    box6.style.transform = `translateX(${scrol} + 'px')`
+let moveToSlide = (track, currentSlide, targetSlide) =>{
+    track.style.transform = 'translateX(-'+ targetSlide.style.left + ')'
+    currentSlide.classList.remove('current')
+    targetSlide.classList.add('current')
+}
+
+dotsnav.addEventListener('click', e => {
+    let targetdot = e.target.closest('button');
+    
+    if (!targetdot){
+        return;
+    }else{
+        let currentSlide = track.querySelector('.current');
+        let currentDot = dotsnav.querySelector('.active');
+        let targetIndex = dots.findIndex(dot => dot === targetdot);
+        let targetSlide = slides[targetIndex];
+        moveToSlide(track, currentSlide, targetSlide);
+
+        currentDot.classList.remove('active')
+        targetdot.classList.add('active');
+        
+    }
 })
 
-scrol2.addEventListener('click', function(){
-    scrol1.classList.remove('active')
-    scrol2.classList.remove('active')
-    scrol3.classList.remove('active')
-    scrol4.classList.remove('active')
-    this.classList.add('active')
 
-    let scrol = reed1.getBoundingClientRect().left
-    box6.style.transform = `translateX(${scrol} + 'px')`
-})
-
-scrol3.addEventListener('click', function(){
-    scrol1.classList.remove('active')
-    scrol2.classList.remove('active')
-    scrol3.classList.remove('active')
-    scrol4.classList.remove('active')
-    this.classList.add('active')
-
-    let scrol = reed1.getBoundingClientRect().left
-    box6.style.transform = `translateX(${scrol} + 'px')`
-})
-
-scrol4.addEventListener('click', function(){
-    scrol1.classList.remove('active')
-    scrol2.classList.remove('active')
-    scrol3.classList.remove('active')
-    scrol4.classList.remove('active')
-    this.classList.add('active')
-
-    let scrol = reed1.getBoundingClientRect().left
-    box6.style.transform = `translateX(${scrol} + 'px')`
-})
